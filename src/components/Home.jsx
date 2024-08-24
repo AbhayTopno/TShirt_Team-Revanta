1dc5ae670ddf11ad5ca2d16b324bb8bc14c7553b
Abhay <abhaytopno999@gmail.com>
2024-08-25 01:19:36 +0530

    Made it Responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate();
+
   const handleButtonClick = () => {
     navigate('/form');
-  }
+  };
+
   return (
     <div className='text-white flex flex-col items-center justify-center h-[450px] w-full gap-8'>
-      <h1 className='bg-gradient-to-r from-green-500 via-lime-500 to-yellow-500 bg-clip-text text-4xl tracking-tight text-transparent'>
-      Official Merch of Team Revanta
+      <h1 className='bg-gradient-to-r from-green-500 via-lime-500 to-yellow-500 bg-clip-text text-4xl tracking-tight text-transparent text-center'>
+        Official Merch <br className='sm:hidden' /> of Team Revanta
       </h1>
-      <h3 className='text-2xl'>Submit your details here</h3>
-      <button 
-      onClick={handleButtonClick}
-      className='bg-green-600 text-xl h-[40px] w-[150px] rounded-lg hover:bg-green-800 transition-all ease-in duration-300'>
+      <h3 className='text-2xl text-center'>Submit your details here</h3>
+      <button
+        onClick={handleButtonClick}
+        aria-label="Navigate to the form"
+        className='bg-green-600 text-xl h-[40px] w-[150px] rounded-lg hover:bg-green-800 transition-all ease-in duration-300'
+      >
         Get Started
-        </button>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   const location = useLocation();
 
   return (
-    <div className='text-green-500 flex justify-between items-center m-10 px-4'>
-      <div className='text-3xl cursor-pointer'>Team Revanta</div>
-      <ul className='flex gap-20 text-lg relative'>
+    <div className='text-green-500 flex flex-col sm:flex-row justify-between items-center m-10 px-4'>
+      <div className='text-3xl cursor-pointer mb-4 sm:mb-0 text-center'>
+        Team Revanta
+      </div>
+      <ul className='flex flex-row gap-4 sm:gap-20 text-lg'>
         <li>
           <Link 
             to="/" 
